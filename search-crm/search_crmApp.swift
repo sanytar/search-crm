a552b3e8bf08b765060c8b1ea8cd64cef760f4c5
aleksandr-ryazanov <a.ryazanov@upbeatsoft.ru>
2026-03-25 08:05:38 +0300

    Initial Commit

---
+++ search-crm/search_crmApp.swift
@@ -0,0 +1,17 @@
+//
+//  search_crmApp.swift
+//  search-crm
+//
+//  Created by Александр Рязанов on 25.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct search_crmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
